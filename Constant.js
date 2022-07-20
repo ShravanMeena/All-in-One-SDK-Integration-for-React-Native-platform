b991ef14b22701bd68d00447a1d4eace338f3063
Shravan <41790742+ShravanMeena@users.noreply.github.com>
2022-07-20 17:44:22 +0530

    Update Constant.js

--- Constant.js
+++ Constant.js
@@ -1,6 +1,6 @@
-export const MID = 'ZHMpwy77672646700865';
+export const MID = '';
 export const API_URL =
-  'https://api.khelgully.com/api/v1/esport/esport_game_contest/paymentinit';
+  '';
 export const CALLBACK_URL =
-  'https://securegw-stage.paytm.in/theia/paytmCallback?ORDER_ID=';
-export const URL_SCHEME = 'paytmMID+jHGcCJ08185342525960';
+  '';
+export const URL_SCHEME = '';
